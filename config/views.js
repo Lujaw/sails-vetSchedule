dfaa9472666444b4a0a6ee5bb8f28a176fea5e71
Lujaw <lujaw@hotmail.com>
2014-01-14 14:43:35 +0545

    templating engine changed to jade

--- config/views.js
+++ config/views.js
@@ -1,8 +1,8 @@
 /**
  * Views
- * 
+ *
  * Server-sent views are a classic and effective way to get your app up and running.
- * Views are normally served from controllers.  Below, you can configure your 
+ * Views are normally served from controllers.  Below, you can configure your
  * templating language/framework of choice and configure Sails' layout support.
  *
  * For more information on views and layouts, check out:
@@ -25,7 +25,7 @@ module.exports.views = {
   // For more options, check out the docs:
   // https://github.com/balderdashy/sails-wiki/blob/0.9/config.views.md#engine
 
-  engine: 'ejs',
+  engine: 'jade',
 
 
 
