a7cf7a95d73f79a542b5ebdf993a3299b25cb327
Lujaw <lujaw@hotmail.com>
2014-01-14 17:29:55 +0545

    user views and model files changed

---
+++ api/controllers/UserController.js
@@ -0,0 +1,151 @@
+/**
+ * UserController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+	"new": function(req, res) {
+		res.view();
+	},
+	/*
+	 * Action blueprints:
+	 *    `/user/index`
+	 *    `/user`
+	 */
+	index: function(req, res, next) {
+		console.log("index action reached");
+		// User.find(function findUsers(err, users) {
+		// 	if (err) return next(err);
+		// 	res.view({
+		// 		users: users
+		// 	});
+		// });
+		res.view();
+	},
+
+
+	/**
+	 * Action blueprints:
+	 *    `/user/create`
+	 
+	create: function(req, res, next) {
+
+		User.create(req.params.all(), function userCreated(err, user) {
+			if (err) {
+				console.log(err);
+				req.session.flash = {
+					err: err
+				}
+				return res.redirect("user/new");
+
+			}
+			res.redirect("/user/show/" + user.id);
+
+		});
+
+	},
+
+
+	//show the user view
+	show: function(req, res, next) {
+		console.log(req.params['id']);
+		User.findOne(req.params['id'], function findUser(err, user) {
+			if (err) return next(err);
+			if (!user) return next();
+			res.view({
+				user: user
+			});
+		})
+	},
+
+	/**
+	 * Action blueprints:
+	 *    `/user/update`
+	 */
+	update: function(req, res, next) {
+
+		var userObj = {
+			name: req.param('name'),
+			title: req.param('title'),
+			email: req.param('email'),
+			admin: req.param('admin')
+		};
+
+		console.log(req.params['id']);
+
+		User.findOne(req.params.id).done(function(err, user) {
+			user.name = req.params.name;
+		})
+
+		User.update(
+			req.param['id'], userObj, function userUpdate(err, user) {
+				if (err) {
+					return res.redirect('/user/edit/' + req.params['id']);
+				}
+				res.redirect('/user/show/' + req.params['id']);
+			});
+		console.log("User has been updated");
+	},
+
+
+	//edit the user
+	edit: function(req, res, next) {
+		//find the user via the id
+		User.findOne(req.params.id, function findUser(err, user) {
+			if (err) return next(err);
+			if (!user) return next("User doesn\'t exists.")
+			// Send a JSON response
+			return res.view({
+				user: user
+			});
+		});
+	},
+
+
+	/**
+	 * Action blueprints:
+	 *    `/user/delete`
+	 */
+	delete: function(req, res) {
+
+		// Send a JSON response
+		return res.json({
+			hello: 'world'
+		});
+	},
+
+
+	/**
+	 * Action blueprints:
+	 *    `/user/search`
+	 */
+	search: function(req, res) {
+
+		// Send a JSON response
+		return res.json({
+			hello: 'world'
+		});
+	},
+
+
+
+	/**
+	 * Overrides for the settings in `config/controllers.js`
+	 * (specific to UserController)
+	 */
+	_config: {}
+
+
+};
---
+++ api/models/User.js
@@ -0,0 +1,42 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+	migrate: 'alter',
+	attributes: {
+
+		name: {
+			type: 'STRING',
+			required: true
+		},
+
+		password: {
+			type: 'STRING',
+			required: true
+		},
+
+		email: {
+			type: 'STRING',
+			required: true
+		},
+
+		address: {
+			type: 'STRING',
+			required: true
+		},
+
+		type: {
+			type: 'STRING',
+			required: true,
+			defaultsTo: "normal"
+		}
+
+	}
+
+};
